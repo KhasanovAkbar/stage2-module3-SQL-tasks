1618f9fd89cc7b1c155954206b3d42c0a1c0b4e9
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-10-23 01:08:16 +0200

    lesson 11

--- lesson11/src/main/resources/SOLUTION.sql
+++ lesson11/src/main/resources/SOLUTION.sql
@@ -1,18 +1,18 @@
-update SUBJECT s
-set s.GRADE = 5
-where s.NAME = 'End of Suburbia: Oil Depletion and the Collapse of the American Dream';
+update SUBJECT
+set GRADE = 5
+where NAME = 'End of Suburbia: Oil Depletion and the Collapse of the American Dream';
 
-update STUDENT s
-set s.GROUPNUMBER = 8
-where s.NAME = 'Tremaine Worvill';
+update STUDENT
+set GROUPNUMBER = 8
+where NAME = 'Tremaine Worvill';
 
-update PAYMENT p
-set p.AMOUNT = 500, p.STUDENT_ID = 2
-where p.PAYMENT_DATE > '2021-01-01' and p.TYPE_ID = 2;
+update PAYMENT
+set AMOUNT = 500, STUDENT_ID = 2
+where PAYMENT_DATE > '2021-01-01' and TYPE_ID = 2;
 
-update MARK m
-set m.MARK = 2
-where m.SUBJECT_ID = 315;
+update MARK
+set MARK = 2
+where SUBJECT_ID = 315;
 
 
 
