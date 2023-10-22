a535be2bc5a275f0d1bb552214395bff00af03fd
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-10-23 01:54:58 +0200

    lesson 8 fixed

--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,7 +1,7 @@
-select min(s.BIRTHDAY) from STUDENT s;
+select max(s.BIRTHDAY) from STUDENT s;
 
 select min(PAYMENT_DATE) from PAYMENT;
 
-select avg(m.MARK) from MARK m;
+select avg(m.MARK) from MARK m join SUBJECT S on S.ID = m.SUBJECT_ID where s.NAME = 'Math';
 
 select min(p.AMOUNT) from PAYMENT p where TYPE_ID = (select id from PAYMENTTYPE where NAME = 'WEEKLY');
