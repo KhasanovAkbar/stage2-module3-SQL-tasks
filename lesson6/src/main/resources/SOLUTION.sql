b8daebadb974a7ca6909225934f6a83d3362b53a
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-10-23 01:48:16 +0200

    lesson 6

--- lesson6/src/main/resources/SOLUTION.sql
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -2,7 +2,7 @@ select * from PAYMENT where TYPE_ID = (select id from PAYMENTTYPE where NAME = '
 
 select * from MARK where SUBJECT_ID = (select ID from SUBJECT where NAME = 'Art');
 
-select s.ID, NAME, BIRTHDAY, GROUPNUMBER from STUDENT s join PAYMENT p on s.ID = p.STUDENT_ID where TYPE_ID = (select id from PAYMENTTYPE pt where pt.NAME = 'WEEKLY');
+SELECT s.ID, s.NAME, s.BIRTHDAY, s.GROUPNUMBER FROM STUDENT s WHERE EXISTS ( SELECT 1 FROM PAYMENT p WHERE p.STUDENT_ID = s.ID AND p.TYPE_ID = (SELECT id FROM PAYMENTTYPE pt WHERE pt.NAME = 'WEEKLY') );
 
 SELECT s.id, s.name, s.birthday, s.GROUPNUMBER FROM Student s JOIN Mark m ON s.id = m.student_id JOIN Subject sub ON m.subject_id = sub.id WHERE sub.name = 'Math';
 
