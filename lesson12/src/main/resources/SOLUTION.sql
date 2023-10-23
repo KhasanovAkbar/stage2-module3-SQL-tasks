bb1a2b87bb92660904f5fe9cb8d7a828f852e1fc
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-10-23 02:30:46 +0200

    single line

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,17 +1,5 @@
-delete from PAYMENT where PAYMENT.STUDENT_ID in (select id from STUDENT where id in (select STUDENT_ID from MARK where MARK > 4));
+delete from PAYMENT where PAYMENT.STUDENT_ID in (select id from STUDENT where id in (select STUDENT_ID from MARK where MARK > 4)); DELETE FROM MARK WHERE student_id IN (SELECT id FROM Student WHERE id IN (SELECT student_id FROM Mark WHERE mark > 4)); DELETE FROM Student WHERE id IN (SELECT id FROM Student WHERE id IN (SELECT student_id FROM Mark WHERE mark > 4));
 
-DELETE FROM MARK WHERE student_id IN (SELECT id FROM Student WHERE id IN (SELECT student_id FROM Mark WHERE mark > 4));
-
-DELETE FROM Student WHERE id IN (SELECT id FROM Student WHERE id IN (SELECT student_id FROM Mark WHERE mark > 4));
-
-SELECT DISTINCT s.id, s.name FROM Student s JOIN Mark m ON s.id = m.student_id WHERE m.mark < 4;
-
-DELETE FROM Student WHERE id IN (SELECT DISTINCT s.id FROM Student s JOIN Mark m ON s.id = m.student_id WHERE m.mark < 4);
-
-DELETE FROM Mark WHERE mark < 4;
-
-delete from PAYMENT where PAYMENT.TYPE_ID in (select ID from PAYMENTTYPE where NAME = 'DAILY');
-
-delete from PAYMENTTYPE where NAME = 'DAILY';
+SELECT DISTINCT s.id, s.name FROM Student s JOIN Mark m ON s.id = m.student_id WHERE m.mark < 4; DELETE FROM Student WHERE id IN (SELECT DISTINCT s.id FROM Student s JOIN Mark m ON s.id = m.student_id WHERE m.mark < 4); DELETE FROM Mark WHERE mark < 4; delete from PAYMENT where PAYMENT.TYPE_ID in (select ID from PAYMENTTYPE where NAME = 'DAILY'); delete from PAYMENTTYPE where NAME = 'DAILY';
 
 delete from MARK where MARK < 7;
