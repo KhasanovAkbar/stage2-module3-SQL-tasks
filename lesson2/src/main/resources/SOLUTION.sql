1c11da35828585937ced53aa9756dd8902d2b48b
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-10-23 02:14:07 +0200

    IDs deleted from tables

--- lesson2/src/main/resources/SOLUTION.sql
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -1,43 +1,48 @@
-insert into Student(id, name,BIRTHDAY, GROUPNUMBER) values (1, 'John',  '2000-01-01',1),
-       (2, 'Chris', '2000-01-01',1),
-       (3, 'Carl',  '2000-01-01',1),
-       (4, 'Oliver','2000-01-01', 2),
-       (5, 'James', '2000-01-01',2),
-       (6, 'Lucas', '2000-01-01',2),
-       (7, 'Henry', '2000-01-01',2),
-       (8, 'Jacob', '2000-01-01',3),
-       (9, 'Logan', '2000-01-01',3),
-       (10, 'Fred', '2000-01-01',4),
-       (11, 'Larry','2000-01-01', 5),
-       (12, 'Andy', '2000-01-01',5);
+insert into Student(name, GROUPNUMBER)
+values ('John', 1),
+       ('Chris', 1),
+       ('Carl', 1),
+       ('Oliver', 2),
+       ('James', 2),
+       ('Lucas', 2),
+       ('Henry', 2),
+       ('Jacob', 3),
+       ('Logan', 3),
+       ('Fred', 4),
+       ('Larry', 5),
+       ('Andy', 5);
 
-insert into Subject(id, name, grade) values (1, 'Art', 1),
-       (2, 'Music', 1),
-       (3, 'Geography', 2),
-       (4, 'History', 2),
-       (5, 'PE', 3),
-       (6, 'Math', 3),
-       (7, 'Science', 4),
-       (8, 'IT', 4),
-       (9, 'Biology', 5),
-       (10, 'English', 5);
+insert into Subject(name, grade)
+values ('Art', 1),
+       ('Music', 1),
+       ('Geography', 2),
+       ('History', 2),
+       ('PE', 3),
+       ('Math', 3),
+       ('Science', 4),
+       ('IT', 4),
+       ('Biology', 5),
+       ('English', 5);
 
-insert into PaymentType(id, name) values (1, 'DAILY'),
-       (2, 'WEEKLY'),
-       (3, 'MONTHLY');
+insert into PaymentType(name)
+values ('DAILY'),
+       ('WEEKLY'),
+       ('MONTHLY');
 
-insert into Payment (ID, TYPE_ID, AMOUNT, STUDENT_ID, PAYMENT_DATE) values (1, 2, 100.0, 1, '2023-10-19'),
-       (2, 3, 200.0, 4, '2023-10-19'),
-       (3, 2, 300.0, 7, '2023-10-19'),
-       (4, 1, 400.0, 5, '2023-10-19'),
-       (5, 3, 500.0, 11, '2023-10-19'),
-       (6, 1, 600.0, 9, '2023-10-19');
+insert into Payment (TYPE_ID, AMOUNT, STUDENT_ID)
+values (2, 100.0, 1,),
+       (3, 200.0, 4,),
+       (2, 300.0, 7,),
+       (1, 400.0, 5,),
+       (3, 500.0, 11),
+       (1, 600.0, 9,);
 
 
-insert into Mark(id, student_id, subject_id, mark) values (1, 2, 1, 8),
-       (2, 4, 4, 5),
-       (3, 9, 3, 9),
-       (4, 8, 6, 4),
-       (5, 9, 5, 9),
-       (6, 12, 3, 5),
-       (7, 7, 2, 3);
+insert into Mark(student_id, subject_id, mark)
+values (2, 1, 8),
+       (4, 4, 5),
+       (9, 3, 9),
+       (8, 6, 4),
+       (9, 5, 9),
+       (12, 3, 5),
+       (7, 2, 3);
--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,12 +1,12 @@
 select * from PAYMENT where AMOUNT >= 500;
 
-select ID, NAME, BIRTHDAY from STUDENT where BIRTHDAY < dateadd(year, -20, current_date);
+select ID, NAME, BIRTHDAY from STUDENT where BIRTHDAY < dateadd(year, -20, current_date());
 
-select ID, NAME, BIRTHDAY, GROUPNUMBER from STUDENT where GROUPNUMBER = 10 and BIRTHDAY > dateadd(year, -20, current_date);
+select ID, NAME, BIRTHDAY, GROUPNUMBER from STUDENT where GROUPNUMBER = 10 and BIRTHDAY > dateadd(year, -20, current_date());
 
 select * from STUDENT where NAME = 'Mike' or GROUPNUMBER in (4, 5, 6);
 
-select * from PAYMENT where PAYMENT_DATE >= dateadd(month, -8, current_date);
+select * from PAYMENT where PAYMENT_DATE >= dateadd(month, -8, current_date());
 
 select * from STUDENT where NAME like 'A%';
 
