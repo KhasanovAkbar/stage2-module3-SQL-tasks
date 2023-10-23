2ef290eb2800b7292b318a8001440f937acf364c
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-10-23 02:55:36 +0200

    single line

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,16 +1,10 @@
 select * from PAYMENT where AMOUNT >= 500;
-
-select ID, NAME, BIRTHDAY from STUDENT where BIRTHDAY < dateadd(year, -20, current_date());
-
-select ID, NAME, BIRTHDAY, GROUPNUMBER from STUDENT where GROUPNUMBER = 10 and BIRTHDAY > dateadd(year, -20, current_date());
-
-select * from STUDENT where NAME = 'Mike' or GROUPNUMBER in (4, 5, 6);
-
-select * from PAYMENT where PAYMENT_DATE >= dateadd(month, -8, current_date());
-
+select ID, NAME, BIRTHDAY from STUDENT where BIRTHDAY < dateadd(year, -20, CURRENT_DATE());
+select ID, NAME, BIRTHDAY, GROUPNUMBER from STUDENT where GROUPNUMBER = 10 AND BIRTHDAY > dateadd(year, -20, CURRENT_DATE());
+select * from STUDENT where NAME = 'Mike' OR GROUPNUMBER IN (4, 5, 6);
+SELECT *FROM Payment WHERE payment_date >= DATEADD(MONTH, -8, CURRENT_DATE());
 select * from STUDENT where NAME like 'A%';
-
-select * from STUDENT where (NAME = 'Roxi' and GROUPNUMBER = 4) or (NAME = 'Tallie' and GROUPNUMBER = 9);
+select * from STUDENT where (NAME = 'Roxi' AND GROUPNUMBER = 4) OR (NAME = 'Tallie' AND GROUPNUMBER = 9);
 
 
 
